0f43f96a1b44c2b6b0789af5a752d9acbbc7d54d
Pavel Polyakov <pavpoly.tch@gmail.com>
2021-03-16 18:57:24 +0300

    app startup

--- app.js
+++ app.js
@@ -251,8 +251,8 @@ app.use(function (req, res, next) {
   res.status(404).render("404");
 });
 
-// Immediate();
-// MainBot();
+Immediate();
+MainBot();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
